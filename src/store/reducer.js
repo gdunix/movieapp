2401a2ab0fb6ebd9c783033174f3184d71a7ae79
Georgios Ntounis <giorgosdounis@gmail.com>
2019-09-20 23:03:29 +0300

    Removed unused reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,18 +2,6 @@ import { combineReducers } from 'redux';
 
 import { reducer as apiReducer } from './middlewares/api';
 
-const rootReducer = (state = {}, action) => {
-    switch (action.type) {
-     case 'SIMPLE_ACTION':
-      return {
-       result: action.payload
-      }
-     default:
-      return state
-    }
-   }
-
 export default combineReducers({
-    rootReducer,
     apiReducer
 });
