830dfe2e8805094b4ed44055b9e7c42fd75161b9
Georgios Ntounis <giorgosdounis@gmail.com>
2021-02-10 01:15:39 +0200

    Fixes in posters styles

--- src/components/posters/styled.js
+++ src/components/posters/styled.js
@@ -3,8 +3,9 @@ import styled from 'styled-components';
 export const List = styled.div`
     display: flex;
     flex-wrap: wrap;
+    justify-content: space-between;
 `;
 
 export const Item = styled.div`
-    margin: 15px 0 0 15px;
+    margin: 15px 0 0 0;
 `;
