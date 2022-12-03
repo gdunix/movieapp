2bd5c28e63ecfe313f97ff580148120ea9332f9e
Georgios Ntounis <giorgosdounis@gmail.com>
2022-12-03 19:43:18 +0200

    Fixed api url

--- src/config/production.js
+++ src/config/production.js
@@ -1,4 +1,4 @@
 export default {
     // baseURL: 'https://thoughtbook-api.herokuapp.com/api/'
-    baseURL: 'https://witty-bull-purse.cyclic.app'
+    baseURL: 'https://witty-bull-purse.cyclic.app/'
 };
